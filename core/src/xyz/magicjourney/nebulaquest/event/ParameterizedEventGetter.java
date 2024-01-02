286d0fb768eb5e9635bb776506dc3c63d6108cd3
Amon De Shir <amonsir29@gmail.com>
2024-01-02 05:56:26 +0100

    Fix event documentation

--- core/src/xyz/magicjourney/nebulaquest/event/ParameterizedEventGetter.java
+++ core/src/xyz/magicjourney/nebulaquest/event/ParameterizedEventGetter.java
@@ -11,24 +11,51 @@ import java.util.function.Consumer;
  * @param <T> The type of the event's parameter.
  */
 public interface ParameterizedEventGetter<T> {
+  
   /**
    * Subscribes to an Event by setting a callback function that will be invoked later 
    * when an event happens.
    * 
    * <p><b>NOTE:</b> A single callback function can subscribe to a particular {@code Event} instance only once.</p>
+   * <p><b>Example:</b></p>
+   * <pre>
+   * // Define a method to be used as a callback
+   * Consumer<T> onEvent = (param) => {
+   *     System.out.println("Event occurred!");
+   * }
+   * 
+   * 
+   * // Subscribe the method for the event
+   * event.subscribe(this.onEvent);
+   * 
+   * // Unsubscribe the method from the event
+   * event.unsubscribe(this.onEvent);
+   * </pre>
    * 
    * @param callback A callback function to be invoked later.
    */
   void subscribe(Consumer<T> callback);
   
   /**
-   * Unsubscribes callback function from the subscribers list, so it will not be called by this event in the future.
-   *
-   * <p><b>NOTE:</b> To unsubscribe from an event, the callback needs to be a reference to the same function that was used when registered.
-   * Anonymous functions like {@code (int x) -> x*x} CANNOT be unsubscribed.
-   * Use {@code this::functionName} instead of {@code () -> this.functionName()}.</p>
+   * Subscribes to an Event by setting a callback function that will be invoked later 
+   * when an event happens.
    * 
-   * @param callback The callback function to be removed from subscribers.
+   * <p><b>NOTE:</b> A single callback function can subscribe to a particular {@code Event} instance only once.</p>
+   * <p><b>Example:</b></p>
+   * <pre>
+   * // Define a method to be used as a callback
+   * Consumer<T> onEvent = (param) => {
+   *     System.out.println("Event occurred!");
+   * }
+   * 
+   * // Subscribe the method for the event
+   * event.subscribe(this.onEvent);
+   * 
+   * // Unsubscribe the method from the event
+   * event.unsubscribe(this.onEvent);
+   * </pre>
+   * 
+   * @param callback A callback function to be invoked later.
    */
   void unsubscribe(Consumer<T> callback);
 }
