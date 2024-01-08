09a5e257e8b8a5b699555e99ee93b8ad128e958e
Dominikaninn <dominikprabucki.2002@gmail.com>
2024-01-08 20:12:30 +0100

    implement hub

--- core/src/xyz/magicjourney/nebulaquest/entity/entities/TeleportHub.java
+++ core/src/xyz/magicjourney/nebulaquest/entity/entities/TeleportHub.java
@@ -5,9 +5,10 @@ import com.badlogic.gdx.assets.AssetManager;
 import xyz.magicjourney.nebulaquest.board.field.BigField;
 import xyz.magicjourney.nebulaquest.board.field.Field;
 import xyz.magicjourney.nebulaquest.entity.Entity;
+import xyz.magicjourney.nebulaquest.entity.Interactiveable;
 import xyz.magicjourney.nebulaquest.player.Player;
 
-public class TeleportHub extends Entity {
+public class TeleportHub extends Entity implements Interactiveable {
   public TeleportHub() {
     super("Teleport Hub", "");
   }
@@ -23,5 +24,15 @@ public class TeleportHub extends Entity {
   @Override
   public Field toField(AssetManager assets) {
     return new BigField(this, "images/hiper-hub", assets);
+  }
+
+  @Override
+  public String getInteractiveablePanelName(Player player) {
+    return "Hub";
+  }
+
+  @Override
+  public boolean isDecisionRequired(Player player) {
+    return false;
   }
 }
